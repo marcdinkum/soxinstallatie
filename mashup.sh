f1f479dfd6074e23a0e80a20dd65ecff17cd76c3
Marc_G <marcg@dinkum.nl>
2015-09-14 14:29:31 +0200

    catting both files

--- mashup.sh
+++ mashup.sh
@@ -3,6 +3,9 @@
 # take a snippet from years.wav
 sox years.wav years_intro.wav trim 9.4 4.3
 
-# reverse the snippet
+# reverse our snippet
 sox years_intro.wav years_rev.wav reverse
+
+# concatenate both fragments
+sox --combine concatenate years_intro.wav years_rev.wav years_mashup.wav
 
